5d57227ef6632491fb7370ab05b8980a74cbdf3e
github username <github email address>
2019-04-22 09:05:03 -0700

    Updates

--- precache-manifest.7af3d174dcb8f2b48b26028f317307a5.js
+++ precache-manifest.7af3d174dcb8f2b48b26028f317307a5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "05e8b090d1989ee18a1d",
+    "revision": "d2aab926f4f1110118f4",
     "url": "/pictionary-online/static/css/main.aaa0168b.chunk.css"
   },
   {
-    "revision": "05e8b090d1989ee18a1d",
-    "url": "/pictionary-online/static/js/main.6260ee17.chunk.js"
+    "revision": "d2aab926f4f1110118f4",
+    "url": "/pictionary-online/static/js/main.b9b24a86.chunk.js"
   },
   {
     "revision": "3f32f27d462ac716118d",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/pictionary-online/static/js/2.abb13db6.chunk.js"
   },
   {
-    "revision": "219b413a49aa00fbc23148e6351afa5c",
+    "revision": "1ff320398640ea8ec2ff00b02ec826d3",
     "url": "/pictionary-online/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pictionary-online/precache-manifest.c894aa8950ebdd7a0208b16d3eadb243.js"
+  "/pictionary-online/precache-manifest.7af3d174dcb8f2b48b26028f317307a5.js"
 );
 
 workbox.clientsClaim();
