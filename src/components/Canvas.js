c80062db5d967a873dfdc0d01db7d7739affdb88
github username <github email address>
2019-04-22 01:30:28 -0700

    added touch methods

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -35,87 +35,6 @@ class Canvas extends React.Component {
     canvas.width = this.state.width;
     canvas.height = this.state.height;
 
-//mobile touch starts
-    // canvas.addEventListener("touchmove", function (e) {
-    //   var touch = e.touches[0];
-    //   var mouseEvent = new MouseEvent("mousemove", {
-    //     clientX: touch.clientX,
-    //     clientY: touch.clientY
-    //   });
-    //   canvas.dispatchEvent(mouseEvent);
-    // }, false);
-    var drawing = false;
-    var mousePos = { x:0, y:0 };
-    var lastPos = mousePos;
-    canvas.addEventListener("mousedown", function (e) {
-            drawing = true;
-      lastPos = getMousePos(canvas, e);
-    }, false);
-    canvas.addEventListener("mouseup", function (e) {
-      drawing = false;
-    }, false);
-    canvas.addEventListener("mousemove", function (e) {
-      mousePos = getMousePos(canvas, e);
-    }, false);
-
-    // Get the position of the mouse relative to the canvas
-    function getMousePos(canvasDom, mouseEvent) {
-      var rect = canvasDom.getBoundingClientRect();
-      return {
-        x: mouseEvent.clientX - rect.left,
-        y: mouseEvent.clientY - rect.top
-      };
-    }
-
-    canvas.addEventListener("touchstart", function (e) {
-        mousePos = getTouchPos(canvas, e);
-    var touch = e.touches[0];
-    var mouseEvent = new MouseEvent("mousedown", {
-      clientX: touch.clientX,
-      clientY: touch.clientY
-    });
-    canvas.dispatchEvent(mouseEvent);
-    }, false);
-    canvas.addEventListener("touchend", function (e) {
-      var mouseEvent = new MouseEvent("mouseup", {});
-      canvas.dispatchEvent(mouseEvent);
-    }, false);
-    canvas.addEventListener("touchmove", function (e) {
-      var touch = e.touches[0];
-      var mouseEvent = new MouseEvent("mousemove", {
-        clientX: touch.clientX,
-        clientY: touch.clientY
-      });
-      canvas.dispatchEvent(mouseEvent);
-    }, false);
-
-    // Get the position of a touch relative to the canvas
-    function getTouchPos(canvasDom, touchEvent) {
-      var rect = canvasDom.getBoundingClientRect();
-      return {
-        x: touchEvent.touches[0].clientX - rect.left,
-        y: touchEvent.touches[0].clientY - rect.top
-      };
-    }
-
-// Prevent scrolling when touching the canvas
-    document.body.addEventListener("touchstart", function (e) {
-      if (e.target == canvas) {
-        e.preventDefault();
-      }
-    }, false);
-    document.body.addEventListener("touchend", function (e) {
-      if (e.target == canvas) {
-        e.preventDefault();
-      }
-    }, false);
-    document.body.addEventListener("touchmove", function (e) {
-      if (e.target == canvas) {
-        e.preventDefault();
-      }
-    }, false);
-//mobile touch ends
-
     this.setState(
       {canvas, ctx},
       // test data to fill the canvas w a rectangle
@@ -263,7 +182,51 @@ class Canvas extends React.Component {
     setTimeout(window.location.reload(), 2000)
   }
 //mobile draw start
+touchStart = (e) =>{
+  // e.preventDefault()
+  this.setState(
+    (state) => {return {
+      prevX: state.currX,
+      prevY: state.currY,
+      currX: e.clientX - state.canvas.offsetLeft,
+      currY: e.clientY - state.canvas.offsetTop,
+      flag: true,
+      dot_flag: true
+    }},
+    () => {
+      if (this.state.dot_flag) {
+        this.state.ctx.beginPath();
+        this.state.ctx.fillStyle = this.state.x;
+        this.state.ctx.fillRect(this.state.currX, this.state.currY, 2, 2);
+        this.state.ctx.closePath();
+        this.setState({dot_flag: false})
+      }
+    }
+  )
+}
 
+touchMove = (e)=>{
+  if(this.state.flag){
+    this.setState(
+      (state) => {
+        return {
+          prevX: state.currX,
+          prevY: state.currY,
+          currX: e.clientX - state.canvas.offsetLeft,
+          currY: e.clientY - state.canvas.offsetTop,
+        }
+      },
+      () => {
+        this.draw()
+      }
+    )
+  }
+}
+
+touchEnd = (e)=>{
+  this.setState({flag: false})
+  this.sendDrawData()
+}
 
   render() {
 
@@ -276,6 +239,9 @@ class Canvas extends React.Component {
             onMouseDown={(event) => this.handleMouseMoves(event, 'down')}
             onMouseUp={(event) => this.handleMouseMoves(event, 'up')}
             onMouseOut={(event) => this.handleMouseMoves(event, 'out')}
+            ontouchstart={this.touchStart}
+            ontouchmove={this.touchMove}
+            ontouchend={this.touchEnd}
           />
           <button onClick={this.handleClear}>Reset</button>
         </div>
